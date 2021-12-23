af457d1596edfaa0c8dec20b6d1537a817d7ef5d
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-12-23 14:11:08 +0100

    Finished learning about Routing in vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Jobs from '../views/Jobs.vue'
 
 const routes = [
   {
@@ -11,13 +12,18 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component:About
+    component: About
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-}) 
+})
 
 export default router
---
+++ src/views/Jobs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-for="job in jobs" :key="job.id">
+    <h5>{{ job.title }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { id: 1, title: "UX/UI designer", desc: "Hire UX/UI designer" },
+        { id: 2, title: "Frontend developer ", desc: "Frontend developer" },
+        { id: 3, title: "UX/UI developer", desc: "Hire UX/UI designer" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+h5 {
+  font-family: sans-serif;
+}
+</style>
