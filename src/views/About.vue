3f09c3f16f9cbeb4681ffbbb63db113ae665828f
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-12-23 13:57:48 +0100

    Finished learning about Router Links in vue

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis,
+      optio ea? Ipsa impedit, minima voluptatem error quia esse facere atque
+      inventore ex debitis quasi vitae nisi consequatur, rerum fugiat suscipit
+      sapiente nostrum, aut corrupti natus deserunt iure. Pariatur omnis
+      asperiores deserunt repellat corrupti blanditiis possimus libero commodi
+      unde sit magnam sapiente distinctio, adipisci eligendi fugiat officiis
+      natus minus consequuntur, sed consectetur rem maxime maiores. Natus iusto
+      esse asperiores dolores quae, illum, hic voluptatibus expedita nisi
+      explicabo deleniti, quia sunt. Eum nostrum incidunt recusandae reiciendis,
+      possimus, impedit perspiciatis dicta harum nisi vero veritatis asperiores
+      repellat inventore dignissimos. Veritatis doloribus minus quisquam.
+    </p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis,
+      optio ea? Ipsa impedit, minima voluptatem error quia esse facere atque
+      inventore ex debitis quasi vitae nisi consequatur, rerum fugiat suscipit
+      sapiente nostrum, aut corrupti natus deserunt iure. Pariatur omnis
+      asperiores deserunt repellat corrupti blanditiis possimus libero commodi
+      unde sit magnam sapiente distinctio, adipisci eligendi fugiat officiis
+      natus minus consequuntur, sed consectetur rem maxime maiores. Natus iusto
+      esse asperiores dolores quae, illum, hic voluptatibus expedita nisi
+      explicabo deleniti, quia sunt. Eum nostrum incidunt recusandae reiciendis,
+      possimus, impedit perspiciatis dicta harum nisi vero veritatis asperiores
+      repellat inventore dignissimos. Veritatis doloribus minus quisquam.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis,
+      optio ea? Ipsa impedit, minima voluptatem error quia esse facere atque
+      inventore ex debitis quasi vitae nisi consequatur, rerum fugiat suscipit
+      sapiente nostrum, aut corrupti natus deserunt iure. Pariatur omnis
+      asperiores deserunt repellat corrupti blanditiis possimus libero commodi
+      unde sit magnam sapiente distinctio, adipisci eligendi fugiat officiis
+      natus minus consequuntur, sed consectetur rem maxime maiores. Natus iusto
+      esse asperiores dolores quae, illum, hic voluptatibus expedita nisi
+      explicabo deleniti, quia sunt. Eum nostrum incidunt recusandae reiciendis,
+      possimus, impedit perspiciatis dicta harum nisi vero veritatis asperiores
+      repellat inventore dignissimos. Veritatis doloribus minus quisquam.
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
