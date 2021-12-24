a69546409e9a2e14be17513b9c470f2ba42b0a4e
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-12-24 19:57:28 +0100

    added API for job id

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,30 @@
 <template>
-  <h3>Job Details Page</h3>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h3>{{ job.title }}</h3>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.desc }}</p>
+  </div>
+
+  <div v-else>
+    <p>Loading job details.......</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+
+  },
 };
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,15 @@
 <template>
   <h2>Jobs</h2>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }"
-      ><h5>{{ job.title }}</h5></router-link
-    >
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }"
+        ><h5>{{ job.title }}</h5></router-link
+      >
+    </div>
+  </div>
+
+  <div v-else>
+    <p>Loading Jobs..</p>
   </div>
 </template>
 
@@ -11,12 +17,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        { id: 1, title: "UX/UI designer", desc: "Hire UX/UI designer" },
-        { id: 2, title: "Frontend developer ", desc: "Frontend developer" },
-        { id: 3, title: "UX/UI developer", desc: "Hire UX/UI designer" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
